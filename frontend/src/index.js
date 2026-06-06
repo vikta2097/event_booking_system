4e7c66425b8331487d0d9764e138e348ef986105
vikta2097 <thigamwangi2027@gmail.com>
2026-06-06 05:42:09 +0300

    theme toggle

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ThemeProvider } from "./components/ThemeContext"; // ← add this
+import { ThemeProvider } from "./pages/ThemeContext"; // ← add this
 
 const queryClient = new QueryClient({
   defaultOptions: {
